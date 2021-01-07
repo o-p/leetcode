2fb409fc6b1254fb5fb348d26d65ec7bb132743e
Chris Chu <op.github.io@gmail.com>
2021-01-07 16:53:05 +0800

    Add #0019 Remove Nth Node From End of List

---
+++ problems/0019-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.test.ts
@@ -0,0 +1,55 @@
+import removeNthFromEnd from './remove-nth-node-from-end-of-list';
+import removeNth from './remove-nth-node-from-end-of-list';
+
+
+describe('Remove Nth Node From End of List', () => {
+  class ListNode {
+    val: number
+    next: ListNode | null
+
+    constructor(val?: number, next?: ListNode | null) {
+      this.val = (val === undefined ? 0 : val)
+      this.next = (next === undefined ? null : next)
+    }
+  }
+
+  test('head = [1], n = 1', (): void => {
+    const head = new ListNode(1);
+    const result = removeNthFromEnd(head, 1);
+    expect(result).toBe(null);
+  });
+
+  test('head = [1,2], n = 1', (): void => {
+    const head = new ListNode(
+      1,
+      new ListNode(2)
+    );
+    const result = removeNthFromEnd(head, 1);
+    expect(result).toBeInstanceOf(ListNode);
+    expect(result.val).toBe(1);
+    expect(result.next).toBeNull();
+  });
+
+  test('head = [1,2,3,4,5], n = 2', (): void => {
+    const head = new ListNode(
+      1,
+      new ListNode(
+        2,
+        new ListNode(
+          3,
+          new ListNode(
+            4,
+            new ListNode(5)
+          )
+        )
+      )
+    );
+    const result = removeNthFromEnd(head, 2);
+    expect(result).toBeInstanceOf(ListNode);
+    expect(result.val).toBe(1);
+    expect(result.next.val).toBe(2);
+    expect(result.next.next.val).toBe(3);
+    expect(result.next.next.next.val).toBe(5);
+    expect(result.next.next.next.next).toBeNull();
+  });
+});
---
+++ problems/0019-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,48 @@
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
+
+// 不斷 next, 直到超過 n 之後就持續迭代替換 node
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  let counts = 0;
+
+  let tail = head;
+  let detached = head;
+  let parent = null;
+  let anchor = head.next;
+
+  //                 n + 1    n        n-1             0
+  // head => ... => parent ========> anchor => ... => tail
+  //                    .. detached ...
+
+  while (tail) {
+    counts++;
+    tail = tail.next;
+
+    if (counts > n) {
+      if (parent) parent.next = detached; // 補接 parent
+
+      // 替換三個主要指標
+      [
+        parent,
+        detached,
+        anchor,
+      ] = [
+        detached,
+        anchor,
+        anchor.next,
+      ];
+
+      // 建立新關聯
+      parent.next = anchor;
+      detached.next = null;
+    }
+  }
+
+  return counts === n
+    ? head.next
+    : head;
+}
+
+export default removeNthFromEnd;
