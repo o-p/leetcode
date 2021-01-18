dedf757fab5b74a6af33d8d4755d3aa5525b3bc7
Chris Chu <op.github.io@gmail.com>
2021-01-18 15:02:21 +0800

    Extract ListNode to helper folder :wheelchair:

---
+++ helpers/ListNode.ts
@@ -0,0 +1,16 @@
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
+
+class ListNode {
+  val: number
+  next: ListNode | null
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+export default ListNode;
